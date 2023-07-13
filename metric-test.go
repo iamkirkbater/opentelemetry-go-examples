29c340bfc9ee40ef06103d511fc5a997cf6c83ff
Kirk Bater <kirk.bater@gmail.com>
2023-07-13 19:23:48 -0400

    wip adding second exp

--- metric-test.go
+++ metric-test.go
@@ -1,85 +1,55 @@
-    //
-    // 	read := metric.NewPeriodicReader(exp, metric.WithInterval(1*time.Second))
-    // 	provider := metric.NewMeterProvider(metric.WithResource(res), metric.WithReader(read))
-    // 	defer func() {
-    // 		err := provider.Shutdown(context.Background())
-    // 		if err != nil {
-    // 			log.Fatal(err)
-    // 		}
-    // 	}()
-    // 	otel.SetMeterProvider(provider)
-    // 	m := provider.Meter("meter-name")
-    //
-    // 	log.Print("registering metric")
-    //
-    // You may obtain a copy of the License at
-    //
-    //     http://www.apache.org/licenses/LICENSE-2.0
-    //
-    // Unless required by applicable law or agreed to in writing, software
-    // distributed under the License is distributed on an "AS IS" BASIS,
-    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    // See the License for the specific language governing permissions and
-    // limitations under the License.
+package main
 
-    //go:build go1.18
-    // +build go1.18
+import (
+	"context"
+	"log"
+	"time"
 
-    package main
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	oltpmetric "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/metric"
+	metricsdk "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
 
-    import (
-        "context"
-        "log"
-        "os"
-        "os/signal"
-        "time"
+var res = resource.NewWithAttributes(
+	semconv.SchemaURL,
+	semconv.ServiceName("runtime-instrumentation-example"),
+)
 
-        "go.opentelemetry.io/otel"
-        "go.opentelemetry.io/otel/attribute"
-        "go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-        "go.opentelemetry.io/otel/metric"
-        metricsdk "go.opentelemetry.io/otel/sdk/metric"
-        "go.opentelemetry.io/otel/sdk/resource"
-        semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-    )
+func main() {
+	stdoutExp, err := stdoutmetric.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Register the exporter with an SDK via a periodic reader.
+	stdoutRead := metricsdk.NewPeriodicReader(stdoutExp, metricsdk.WithInterval(1*time.Second))
 
-    var res = resource.NewWithAttributes(
-        semconv.SchemaURL,
-        semconv.ServiceName("runtime-instrumentation-example"),
-    )
+	secureOpt := oltpmetric.WithInsecure()
 
-    func main() {
-        exp, err := stdoutmetric.New()
-        if err != nil {
-            log.Fatal(err)
-        }
+	//read := metricsdk.NewManualReader()
+	provider := metricsdk.NewMeterProvider(metricsdk.WithResource(res), metricsdk.WithReader(stdoutRead))
+	defer func() {
+		err := provider.Shutdown(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	otel.SetMeterProvider(provider)
 
-        // Register the exporter with an SDK via a periodic reader.
-        read := metricsdk.NewPeriodicReader(exp, metricsdk.WithInterval(1*time.Second))
-        provider := metricsdk.NewMeterProvider(metricsdk.WithResource(res), metricsdk.WithReader(read))
-        defer func() {
-            err := provider.Shutdown(context.Background())
-            if err != nil {
-                log.Fatal(err)
-            }
-        }()
-        otel.SetMeterProvider(provider)
+	log.Print("Starting runtime instrumentation:")
+	m := otel.Meter("my.meter.name")
 
-        ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-        defer cancel()
+	counter, _ := m.Int64Counter(
+		"some.prefix.counter",
+		metric.WithDescription("my-counter"),
+		metric.WithUnit("calls"),
+	)
 
-        log.Print("Starting runtime instrumentation:")
-        m := otel.Meter("my.meter.name")
-
-        counter, _ := m.Int64Counter(
-            "some.prefix.counter",
-            metric.WithDescription("my-counter"),
-            metric.WithUnit("calls"),
-        )
-
-        counter.Add(ctx, 1, metric.WithAttributes(
-			attribute.String("cmd", "root")),
-		)
-
-        <-ctx.Done()
-    }
+	counter.Add(context.TODO(), 1, metric.WithAttributes(
+		attribute.String("cmd", "root")),
+	)
+}
