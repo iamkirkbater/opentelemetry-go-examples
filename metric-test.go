4b3ad8876857ff3192de2ad2a3aa8379dfbcec92
Kirk Bater <kirk.bater@gmail.com>
2023-07-13 18:22:44 -0400

    periodic-reader complete example

--- metric-test.go
+++ metric-test.go
@@ -1,67 +1,85 @@
-// Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+    //
+    // 	read := metric.NewPeriodicReader(exp, metric.WithInterval(1*time.Second))
+    // 	provider := metric.NewMeterProvider(metric.WithResource(res), metric.WithReader(read))
+    // 	defer func() {
+    // 		err := provider.Shutdown(context.Background())
+    // 		if err != nil {
+    // 			log.Fatal(err)
+    // 		}
+    // 	}()
+    // 	otel.SetMeterProvider(provider)
+    // 	m := provider.Meter("meter-name")
+    //
+    // 	log.Print("registering metric")
+    //
+    // You may obtain a copy of the License at
+    //
+    //     http://www.apache.org/licenses/LICENSE-2.0
+    //
+    // Unless required by applicable law or agreed to in writing, software
+    // distributed under the License is distributed on an "AS IS" BASIS,
+    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    // See the License for the specific language governing permissions and
+    // limitations under the License.
 
-//go:build go1.18
-// +build go1.18
+    //go:build go1.18
+    // +build go1.18
 
-package main
+    package main
 
-import (
-	"context"
-	"log"
-	"os"
-	"os/signal"
-	"time"
+    import (
+        "context"
+        "log"
+        "os"
+        "os/signal"
+        "time"
 
-	"go.opentelemetry.io/otel"
-	metricExporter "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
-	metricOpts "go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-)
+        "go.opentelemetry.io/otel"
+        "go.opentelemetry.io/otel/attribute"
+        "go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+        "go.opentelemetry.io/otel/metric"
+        metricsdk "go.opentelemetry.io/otel/sdk/metric"
+        "go.opentelemetry.io/otel/sdk/resource"
+        semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+    )
 
-var res = resource.NewWithAttributes(
-	semconv.SchemaURL,
-	semconv.ServiceName("occ"),
-)
+    var res = resource.NewWithAttributes(
+        semconv.SchemaURL,
+        semconv.ServiceName("runtime-instrumentation-example"),
+    )
 
-func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
+    func main() {
+        exp, err := stdoutmetric.New()
+        if err != nil {
+            log.Fatal(err)
+        }
 
-	exp, err := metricExporter.New(ctx, metricExporter.WithInsecure(), metricExporter.WithEndpoint("localhost:4318"))
-	if err != nil {
-		log.Fatal(err)
-	}
+        // Register the exporter with an SDK via a periodic reader.
+        read := metricsdk.NewPeriodicReader(exp, metricsdk.WithInterval(1*time.Second))
+        provider := metricsdk.NewMeterProvider(metricsdk.WithResource(res), metricsdk.WithReader(read))
+        defer func() {
+            err := provider.Shutdown(context.Background())
+            if err != nil {
+                log.Fatal(err)
+            }
+        }()
+        otel.SetMeterProvider(provider)
 
-	read := metric.NewPeriodicReader(exp, metric.WithInterval(1*time.Second))
-	provider := metric.NewMeterProvider(metric.WithResource(res), metric.WithReader(read))
-	defer func() {
-		err := provider.Shutdown(context.Background())
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	otel.SetMeterProvider(provider)
-	m := provider.Meter("meter-name")
+        ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+        defer cancel()
 
-	log.Print("registering metric")
-	counter, _ := m.Int64Counter(
-		"some.prefix.counter",
-		metricOpts.WithDescription("test description"),
-	)
+        log.Print("Starting runtime instrumentation:")
+        m := otel.Meter("my.meter.name")
 
-	counter.Add(ctx, 1)
-}
+        counter, _ := m.Int64Counter(
+            "some.prefix.counter",
+            metric.WithDescription("my-counter"),
+            metric.WithUnit("calls"),
+        )
+
+        counter.Add(ctx, 1, metric.WithAttributes(
+			attribute.String("cmd", "root")),
+		)
+
+        <-ctx.Done()
+    }
